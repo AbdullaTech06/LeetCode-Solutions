6984f8e7289d289966849f5ed9a75181082b219e
Abdulla Ahmed <adduahm@gmail.com>
2026-02-12 18:45:42 +0530

    Time: 0 ms (100%), Space: 7.9 MB (64.33%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int cnt=0;
+        while(n>1){
+            if(n%2==0){
+                n/=2;
+                cnt+=n;
+            }else{
+                n=(n-1)/2+1;
+                cnt+=n-1;
+            }
+        }
+        return cnt;
+    }
+};
