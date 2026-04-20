4bbe6ea1fd55694ba5c16f35b913e87ebfcf658b
Abdulla Ahmed <adduahm@gmail.com>
2026-04-20 20:58:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maxDepth(string s) {
+        //mei yahn pe stack kyu use kiya?
+        //yeh mujhe bhi nhi pata
         stack<char> st;
         int ans =0;
         for(char ch : s){
