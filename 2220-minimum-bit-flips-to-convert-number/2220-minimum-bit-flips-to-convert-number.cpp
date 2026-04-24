c6a09ab73d709d54ad3ad6cbeb52ae13a4ae78ee
Abdulla Ahmed <adduahm@gmail.com>
2026-04-24 21:25:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int x = start^goal;
-        int cnt=0;
-        while(x>0){
-            if(x%2==1){
-                cnt++;
-            }
-            x/=2;
-        }
-        return cnt;
+        return __builtin_popcount(start ^ goal);
     }
 };
