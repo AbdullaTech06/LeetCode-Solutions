757b70cf7501e4485251a1c9620126fc93c09fc3
Abdulla Ahmed <adduahm@gmail.com>
2026-05-21 15:04:30 +0530

    Time: 0 ms (100%), Space: 8.1 MB (74.24%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int mn=0;
+        int mx=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                mn++;
+                mx++;
+            }else if(s[i]==')'){
+                if(mx==0)return false;
+                mn--;
+                mx--;
+            }else{
+                mn--;
+                mx++;
+            }
+            if(mn<0)mn=0;
+            if(mx<0)return false;
+        }
+        return mn==0;
+    }
+};
