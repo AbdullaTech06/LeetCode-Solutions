f0bb3431ec2c785f3d038e0a6db8353b5e7e7dc9
Abdulla Ahmed <adduahm@gmail.com>
2026-06-18 11:43:23 +0530

    Time: 0 ms (100%), Space: 8.2 MB (46.25%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -3,7 +3,7 @@ public:
     double angleClock(int hour, int minutes) {
         double angleminutes=6*(minutes%60);
         double anglehour=30*(hour%12)+(angleminutes/360)*30;
-        double d=max(anglehour,angleminutes)-min(anglehour,angleminutes);
+        double d=abs(angleminutes-anglehour);
         return min(d,360-d);
     }
 };
