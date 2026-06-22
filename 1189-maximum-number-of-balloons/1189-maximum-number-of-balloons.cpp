dc22f4c6b472ee4827c072948112b0465c559c6f
Abdulla Ahmed <adduahm@gmail.com>
2026-06-22 12:00:00 +0530

    Time: 0 ms (100%), Space: 9 MB (39.61%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -5,19 +5,6 @@ public:
         for(auto it:text){
             m[it]++;
         }
-        int ans=0;
-        while(1){
-            if(m['b']>=1 && m['a']>=1 && m['l']>=2 && m['o']>=2 && m['n']>=1){
-                ans++;
-                m['b']-=1;
-                m['a']-=1;
-                m['l']-=2;
-                m['o']-=2;
-                m['n']-=1;
-            }else{
-                break;
-            }
-        }
-        return ans;
+        return min({m['b'],m['a'],m['l']/2,m['o']/2,m['n']});
     }
 };
