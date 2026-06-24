866742c64575ccd7aa8ca59def0ae7e9be3566ae
Abdulla Ahmed <adduahm@gmail.com>
2026-06-24 23:18:20 +0530

    Time: 551 ms (72.09%), Space: 73.3 MB (16.28%) - LeetHub

---
+++ 3700-number-of-zigzag-arrays-ii/3700-number-of-zigzag-arrays-ii.cpp
@@ -0,0 +1,83 @@
+class Solution {
+public:
+    static constexpr long long MOD =1e9+7;
+
+    using Matrix=vector<vector<long long>>;
+
+    Matrix multiply(const Matrix& A, const Matrix& B) {
+        int n=A.size();
+        Matrix C(n, vector<long long>(n, 0));
+
+        for(int i=0;i<n;i++){
+            for(int k=0;k<n;k++){
+                if(A[i][k]==0)continue;
+                for(int j=0;j<n;j++) {
+                    if(B[k][j]==0)continue;
+                    C[i][j]=(C[i][j] +A[i][k] * B[k][j])%MOD;
+                }
+            }
+        }
+        return C;
+    }
+
+    Matrix power(Matrix base, long long exp) {
+        int n=base.size();
+        Matrix res(n,vector<long long>(n,0));
+
+        for(int i=0;i<n;i++)res[i][i] = 1;
+
+        while(exp){
+            if(exp&1)res=multiply(res, base);
+            base=multiply(base, base);
+            exp>>=1;
+        }
+        return res;
+    }
+
+    int zigZagArrays(int n, int l, int r) {
+        int m =r-l+1;
+
+        if(n==1)return m;
+
+        if(n==2)return (1LL*m*(m-1))%MOD;
+
+        int S =2*m;
+
+        Matrix T(S, vector<long long>(S,0));
+
+        for(int x=0;x<m;x++){
+            for(int y =0;y<x;y++){
+                T[x][m+y]=1;
+            }
+        }
+
+        for(int x=0; x<m;x++){
+            for(int y=x+1;y<m;y++){
+                T[m+x][y]=1;
+            }
+        }
+
+        vector<long long> init(S, 0);
+
+        for(int b=0;b<m;b++){
+            init[b]=b;            
+            init[m+b]=m-1-b;
+            }
+
+        Matrix P=power(T,n-2);
+
+        vector<long long> final(S,0);
+
+        for(int j=0;j<S;j++){
+            for(int i=0;i<S;i++){
+                final[j] = (final[j]+init[i]*P[i][j])%MOD;
+            }
+        }
+
+        long long ans= 0;
+        for(long long x:final)
+            ans = (ans+x)%MOD;
+
+        return ans;
+    }
+};
