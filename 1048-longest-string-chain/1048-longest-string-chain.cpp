acd899335bbef79d3ab24306102c04e6b70e0587
Abdulla Ahmed <adduahm@gmail.com>
2026-07-01 12:53:33 +0530

    Time: 83 ms (42.1%), Space: 29.6 MB (39.59%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public: 
+    bool check(string &a,string&b){
+        if(b.size()!=a.size()+1)return false;
+        
+        int i=0;
+        int j=0;
+        while(j<b.size()){
+            if(i<a.size() && a[i]==b[j]){
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+        }
+        return i==a.size();
+    }
+
+    static bool cmp(string a,string b){
+        return a.size()<b.size();
+    }
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(),words.end(),cmp);
+        int n=words.size();
+        vector<int> dp(n,1);
+        int ans=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(check(words[j],words[i])){
+                    dp[i]=max(dp[i],dp[j]+1);
+                }
+            }
+            ans=max(ans,dp[i]);
+        }
+        return ans;
+    }
+};
